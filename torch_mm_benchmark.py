26d7950c1da2cc1e05a7f2476f42f344ad430595
pipecat <pipecat@yahoo.com>
2023-11-20 23:14:57 +0800

    fix: print m n k mistake

--- torch_mm_benchmark.py
+++ torch_mm_benchmark.py
@@ -9,8 +9,8 @@ repeats = 1000
 dtype = [torch.float16, torch.float32, torch.float64]
 devices = ["cuda:0"]
 
+
 def run_benchmark(m, k, n, repeats, dtype, device):
-    
     a = torch.rand((m, k), dtype=dtype, device=device)
     b = torch.rand((k, n), dtype=dtype, device=device)
     c = torch.empty((m, n), dtype=dtype, device=device)
@@ -23,18 +23,22 @@ def run_benchmark(m, k, n, repeats, dtype, device):
 
     time_cost = end_time - start_time
 
-    print("============={}+{}=============".format(device.split(':')[0], d.__repr__().split('.')[1]))
+    print(
+        "============={}+{}=============".format(
+            device.split(":")[0], d.__repr__().split(".")[1]
+        )
+    )
 
     print(" Time cost: {}s".format(time_cost))
 
     print(
         " M: {} \n K: {} \n N: {} \n Repeats: {} \n {} TFLOPS \n".format(
-            m, n, k, repeats, 2 * m * n * k * repeats / time_cost / 1e12
+            m, k, n, repeats, 2 * m * n * k * repeats / time_cost / 1e12
         )
     )
+
 
 if __name__ == "__main__":
     for d in dtype:
         for device in devices:
             run_benchmark(m, k, n, repeats, d, device)
-    
